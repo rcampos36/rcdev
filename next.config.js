5bb583bca0ace3c0fe03b3955caea837396ce3ce
Roger Campos <rcrogercampos@gmail.com>
2026-01-23 15:24:10 -0500

    adding app

--- next.config.js
+++ next.config.js
@@ -11,8 +11,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  // Performance optimizations
-  swcMinify: true,
   // Security headers
   async headers() {
     return [
